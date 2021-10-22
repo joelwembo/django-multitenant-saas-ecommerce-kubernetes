e4f51894c29c8c596b24501eac48e13c53783ace
joel wembo <joelotepawembo@gmail.com>
2021-10-22 22:51:00 +0800

    react app fixing errors

--- drwebapp/src/components/logout.js
+++ drwebapp/src/components/logout.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line
+import React, { useEffect } from 'react';
 import axiosInstance from '../axios';
 import { useHistory } from 'react-router-dom';
 
@@ -6,9 +7,9 @@ export default function SignUp() {
 	const history = useHistory();
 
 	useEffect(() => {
-		const response = axiosInstance.post('user/logout/blacklist/', {
-			refresh_token: localStorage.getItem('refresh_token'),
-		});
+		//const response = axiosInstance.post('user/logout/blacklist/', {
+		//	refresh_token: localStorage.getItem('refresh_token'),
+		//});
 		localStorage.removeItem('access_token');
 		localStorage.removeItem('refresh_token');
 		axiosInstance.defaults.headers['Authorization'] = null;
