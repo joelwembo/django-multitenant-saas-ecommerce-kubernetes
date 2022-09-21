dc7afdbc2f685cb5f2d63f3c44f3f4b122c91c07
Joel Wembo <joelotepawembo@gmail.com>
2022-09-21 23:01:44 +0800

    updated service

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# urls for the entire project
+# urls for the entire project #
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
