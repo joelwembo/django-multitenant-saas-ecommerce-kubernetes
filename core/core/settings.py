ded4929722d991d346e20c0238d236beb925e25e
Joel Wembo <joelotepawembo@gmail.com>
2022-09-26 22:56:57 +0800

    updated code development

--- core/core/settings.py
+++ core/core/settings.py
@@ -6,7 +6,7 @@ from os import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production  / Dev
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
