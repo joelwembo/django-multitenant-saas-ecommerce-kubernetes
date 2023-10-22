3a2177431ccdb998fe8314a64327a96291ce03ce
Joel Wembo <joelotepawembo@gmail.com>
2023-10-23 01:47:58 +0800

    removed notes api app

--- apps/node_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NodeApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.node_api"
--- apps/node_api/tests.py
+++
@@ -1,5 +0,0 @@
-import django
-django.setup()
-
-def test_foo():
-    assert True
