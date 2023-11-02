351909012c38ea5ce9b5d931485d26376ef4fa54
Joel Wembo <19718580+joelwembo@users.noreply.github.com>
2023-11-02 14:31:01 +0800

    update django project

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Python runtime as a parent image
-FROM python:3.9
+FROM python:3.11-slim-bullseye
 # Set environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker-compose down
-# docker-compose build
-docker-compose build --no-cache
-docker-compose --compatibility up
+docker-compose build
+# docker-compose build --no-cache
+docker-compose  up
 
