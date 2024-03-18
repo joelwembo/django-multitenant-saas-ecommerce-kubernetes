2f3a3d3aee2985c902dd3b75d559e948c8f42346
Joel Wembo <joelwembo@outlook.com>
2024-03-19 02:43:53 +0800

    added django api

--- multitenantsaas/settings.py
+++ multitenantsaas/settings.py
@@ -151,26 +151,26 @@ WSGI_APPLICATION = 'multitenantsaas.wsgi.application'
 # config.DATABASE_URL = 'bolt://neo4j+s://f89c638e.databases.neo4j.io:7687'
 
 # SQLite Database 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
 
 # Database postgres Docker 
 # Docker host : host.docker.internal  or db cloudapp-django-postgresdb  3.0.55.190
 # docker inspect cloudapp-django-postgresdb | grep "IPAddress"
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_NAME", "DB2"),
-        'USER': os.environ.get("POSTGRES_USER", "postgres"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
-        'PORT': int(os.environ.get("POSTGRES_PORT", "5432")),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("POSTGRES_NAME", "DB2"),
+#         'USER': os.environ.get("POSTGRES_USER", "postgres"),
+#         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
+#         'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+#         'PORT': int(os.environ.get("POSTGRES_PORT", "5432")),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
