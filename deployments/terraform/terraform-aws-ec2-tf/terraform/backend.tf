3bff51ce9f88434b8cec33cb42a860be7a57b89b
Joel Wembo <joelwembo@outlook.com>
2024-04-12 03:19:44 +0800

    added security group and domain names

--- deployments/terraform/terraform-aws-ec2-tf/terraform/backend.tf
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/backend.tf
@@ -1,21 +1,21 @@
-# terraform {
-#   backend "remote" {  
-#     hostname="app.terraform.io"  
-#     organization = "prodxcloud" 
-#     workspaces {
-#       prefix = "prodxcloud" 
-#     }
-#   }
-# }
-
 terraform {
-  backend "s3" {
-    bucket         = "django-app-9"
-    region         = "us-east-1"
-    key            = "state/terraform.tfstate"
-    # dynamodb_table = "data_onents_tf_lockid"
-    encrypt        = true
+  backend "remote" {  
+    hostname="app.terraform.io"  
+    organization = "prodxcloud" 
+    workspaces {
+      prefix = "prodxcloud-" 
+    }
   }
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "django-app-9"
+#     region         = "us-east-1"
+#     key            = "state/terraform.tfstate"
+#     # dynamodb_table = "data_onents_tf_lockid"
+#     encrypt        = true
+#   }
+# }
 
 
