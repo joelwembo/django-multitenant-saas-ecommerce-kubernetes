1bece52f5e8b98c60dbd98a1c89c3fbf8fa5542c
Joel Wembo <joelwembo@outlook.com>
2024-04-13 20:03:41 +0800

    updated files

--- deployments/deploy-aws-eks-cluster.sh
+++ deployments/deploy-aws-eks-cluster.sh
@@ -12,10 +12,10 @@ PORT=80
 export KUBECONFIG="$KUBECONFIG"
 
 # Apply Kubernetes manifests
-kubectl apply -f k8s/deployment.yaml
+kubectl apply -f deployments/k8s/deployment.yaml
 
 # Update the Docker image in the deployment
 kubectl set image deployment/$DEPLOYMENT_NAME $DEPLOYMENT_NAME=$DOCKER_IMAGE -n $NAMESPACE
 
 # Expose the deployment as a service
-kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=$PORT --target-port=$PORT -n $NAMESPACE --name=$SERVICE_NAME
+kubectl expose deployment cloudapp-django-web --type=LoadBalancer --port=80 --target-port=8585-n $NAMESPACE --name=cloudapp-django-web
