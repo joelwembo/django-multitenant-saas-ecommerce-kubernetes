9ab9dc637fbe4d796637ed7b81d044c9e8190dae
Joel Wembo <joelwembo@outlook.com>
2024-04-24 02:19:06 +0800

    added workflows

--- deployments/terraform/terraform-aws-ec2-tf/terraform/installer.sh
+++ deployments/terraform/terraform-aws-ec2-tf/terraform/installer.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Java Installations
-# sudo apt-get install openjdk-11-jdk -y
-# sudo apt-get install zip -y
-# echo 'JDK Installed successfully installer'
+Java Installations
+sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install zip -y
+echo 'JDK Installed successfully installer'
 
 
-# # Jenkins installations
-# sudo apt update
-# apt install make
-# sudo apt-get install debian-keyring debian-archive-keyring --assume-yes
-# sudo apt-key update
-# sudo apt-get update
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
-# sudo apt update
-# sudo apt install openjdk-11-jre-headless --assume-yes
-# sudo java -version
-# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-# sudo apt-get update
-# sudo apt-get install jenkins --assume-yes
-# # sudo service jenkins status
-# echo 'Jenkins successfully installer : password '
+# Jenkins installations
+sudo apt update
+apt install make
+sudo apt-get install debian-keyring debian-archive-keyring --assume-yes
+sudo apt-key update
+sudo apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
+sudo apt update
+sudo apt install openjdk-11-jre-headless --assume-yes
+sudo java -version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins --assume-yes
+# sudo service jenkins status
+echo 'Jenkins successfully installer'
 # # Postgres Installation
 # sudo apt update
 # # enable repository to install postgresql
